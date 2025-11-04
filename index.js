34058696c3c268dc07a5aca628943975d6f8c069
snehaltayade16 <snehaltayade1603@gmail.com>
2025-11-04 19:33:07 +0530

    updateCode

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const menu = document.getElementById('menu')
 
 humberger.addEventListener('click',() => {
     menu.classList.add('active');
-    menu.classList.remove('inActive');
 })
 
 const close = document.getElementById('closeMenu')
 close.addEventListener('click',() =>{
-    menu.classList.add('inActive');
     menu.classList.remove('active');
 })
