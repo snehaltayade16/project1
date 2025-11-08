452a0ff55674dba39632297f858c02f178581feb
komalgawai1gen <komalgawai@1gen.io>
2025-11-09 00:31:55 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+console.log('loadd')
 const humberger = document.getElementById('hamburger')
 const menu = document.getElementById('menu')
+const overlay = document.getElementById('overlay')
+const a = document.getElementById('a')
+const searchInput = document.getElementById('searchInputId')
+
+console.log(searchInput)
 
 humberger.addEventListener('click',() => {
     menu.classList.add('active');
+    overlay.classList.add('overlayActive')
 })
 
 const close = document.getElementById('closeMenu')
 close.addEventListener('click',() =>{
     menu.classList.remove('active');
+    overlay.classList.remove('overlayActive')
+})
+
+a.addEventListener('click',() => {
+    searchInput.classList.toggle('enlarge')
 })
